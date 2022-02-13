a1634403afa8df22d288943977bd1a51ce525128
amykosandiak <98989590+amykosandiak@users.noreply.github.com>
2022-02-13 19:06:30 +1030

    Added alt text to image

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ function showWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+  currentWeatherIcon.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(event) {
